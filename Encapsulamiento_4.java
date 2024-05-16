f4b597a335aaf4708f0d22dbada8492b510937da
assasinjohan2 <136901152+assasinjohan2@users.noreply.github.com>
2024-05-16 18:57:52 -0500

    Add files via upload

---
+++ Encapsulamiento_4.java
@@ -0,0 +1,10 @@
+public class Encapsulamiento_4 {
+    
+}
+/*
+ * oCrea una clase base FiguraGeometricacon propiedades privadas para el nombre y el color de la figura.
+ * oImplementa métodos públicos establecerNombre(), obtenerNombre(), establecerColor()y obtenerColor()para manipular estas propiedades de manera segura1.
+ * oCrea subclases como Rectangulo, Circulo, etc., que hereden de FiguraGeometricay tengan propiedades adicionales como longitud, ancho, radio, etc.
+ * oAgrega métodos para calcular el área y el perímetro de las figuras geométricas.
+ * 
+ */
